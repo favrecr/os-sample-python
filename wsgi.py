d55f2db2cde48b0fd5d931d949a5c594184a4652
Esteban <eariasn@us.ibm.com>
2020-06-02 15:59:23 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Esteban, this Openshift is working great!"
 
 if __name__ == "__main__":
     application.run()
